538dd2359f2d3ffc29ab6d583f443d85ba8dba0a
Juhi Pathak <73741643+JuhiPathak23@users.noreply.github.com>
2024-08-12 21:53:13 +0530

    Create Ninja's Training.cpp

---
+++ Self Practice-DP/Ninja's Training.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    int maximumPoints(vector<vector<int>>& points, int n) {
+        // Code here
+        vector<int> prev(4, 0);
+        prev[0] = max(points[0][1], points[0][2]);
+        prev[1] = max(points[0][0], points[0][2]);
+        prev[2] = max(points[0][0], points[0][1]);
+        prev[3] = max(points[0][0], max(points[0][1], points[0][2]));
+        for (int day = 1; day < n; day++) {
+            vector<int> temp(4, 0);
+            for (int last = 0; last < 4; last++) {
+                temp[last] = 0;
+                for (int task = 0; task <= 2; task++) {
+                    if (task != last) {
+                        temp[last] = max(temp[last], points[day][task] + prev[task]);
+                    }
+                }
+            }
+            prev = temp;
+        }
+        return prev[3];
+    }
+};

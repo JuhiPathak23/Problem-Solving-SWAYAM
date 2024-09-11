0caf1d531fd4100b744c0fd466662cd264435be1
Juhi Pathak <73741643+JuhiPathak23@users.noreply.github.com>
2024-09-11 21:15:33 +0530

    Update Ninja's Training.cpp

--- Self Practice-DP/Ninja's Training.cpp
+++ Self Practice-DP/Ninja's Training.cpp
@@ -9,7 +9,7 @@ class Solution {
         prev[3] = max(points[0][0], max(points[0][1], points[0][2]));
         for (int day = 1; day < n; day++) {
             vector<int> temp(4, 0);
-            for (int last = 0; last < 4; last++) {
+            for (int last = 0;last < 4; last++) {
                 temp[last] = 0;
                 for (int task = 0; task <= 2; task++) {
                     if (task != last) {
